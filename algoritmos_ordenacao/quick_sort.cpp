f67e04e8eec4f5fbeb858f772947abbf778e7163
PedroGH96 <pdalves30@gmail.com>
2025-07-04 18:05:06 -0300

    Create quick_sort.cpp

---
+++ algoritmos_ordenacao/quick_sort.cpp
@@ -0,0 +1,27 @@
+#include "quick_sort.hpp"
+
+int partition(std::vector<int>& arr, int low, int high, QuickSortMetrics& metrics) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; ++j) {
+        metrics.comparisons++;
+        if (arr[j] < pivot) {
+            i++;
+            std::swap(arr[i], arr[j]);
+            metrics.swaps++;
+        }
+    }
+    std::swap(arr[i + 1], arr[high]);
+    metrics.swaps++;
+    return (i + 1);
+}
+
+void quickSort(std::vector<int>& arr, int low, int high, QuickSortMetrics& metrics) {
+    if (low < high) {
+        int pi = partition(arr, low, high, metrics);
+        quickSort(arr, low, pi - 1, metrics);
+        quickSort(arr, pi + 1, high, metrics);
+    }
+}
+
