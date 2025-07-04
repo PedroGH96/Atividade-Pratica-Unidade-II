657e611cce5f5cba64217feb1cd1c9683ae7972e
PedroGH96 <pdalves30@gmail.com>
2025-07-04 17:52:05 -0300

    Update bubble_sort.cpp

--- algoritmos_ordenacao/bubble_sort.cpp
+++ algoritmos_ordenacao/bubble_sort.cpp
@@ -1,1 +1,16 @@
+#include "bubble_sort.hpp"
+
+void bubbleSort(std::vector<int>& arr, BubbleSortMetrics& metrics) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            metrics.comparisons++;
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+                metrics.swaps++;
+            }
+        }
+    }
+}
+
 
