8de362471cca4edd19fcb676dd86e772df66a247
PedroGH96 <pdalves30@gmail.com>
2025-07-08 19:30:51 -0300

    Update bubble_sort.hpp

--- algoritmos_ordenacao/bubble_sort.hpp
+++ algoritmos_ordenacao/bubble_sort.hpp
@@ -9,19 +9,7 @@ struct BubbleSortMetrics {
     long long swaps;
 
     BubbleSortMetrics() : comparisons(0), swaps(0) {}
-};
-
-void bubbleSort(std::vector<int>& arr, BubbleSortMetrics& metrics) {
-    int n = arr.size();
-    for (int i = 0; i < n - 1; ++i) {
-        for (int j = 0; j < n - i - 1; ++j) {
-            metrics.comparisons++;
-            if (arr[j] > arr[j + 1]) {
-                std::swap(arr[j], arr[j + 1]);
-                metrics.swaps++;
-            }
-        }
-    }
+ };
 }
 
 #endif 
