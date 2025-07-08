a237317437ffa9c81fe79a040f1ae7d32d6c5236
PedroGH96 <pdalves30@gmail.com>
2025-07-08 19:36:01 -0300

    Update selection_sort.hpp

--- algoritmos_ordenacao/selection_sort.hpp
+++ algoritmos_ordenacao/selection_sort.hpp
@@ -9,23 +9,7 @@ struct SelectionSortMetrics {
     long long swaps;
 
     SelectionSortMetrics() : comparisons(0), swaps(0) {}
-};
-
-void selectionSort(std::vector<int>& arr, SelectionSortMetrics& metrics) {
-    int n = arr.size();
-    for (int i = 0; i < n - 1; ++i) {
-        int min_idx = i;
-        for (int j = i + 1; j < n; ++j) {
-            metrics.comparisons++;
-            if (arr[j] < arr[min_idx]) {
-                min_idx = j;
-            }
-        }
-        if (min_idx != i) {
-            std::swap(arr[i], arr[min_idx]);
-            metrics.swaps++;
-        }
-    }
+  };
 }
 
 #endif 
