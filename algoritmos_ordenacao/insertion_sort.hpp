4908ed3fac7df80dd1c1ca93db38b89211b70c43
PedroGH96 <pdalves30@gmail.com>
2025-07-08 09:57:12 -0300

    Update insertion_sort.hpp

--- algoritmos_ordenacao/insertion_sort.hpp
+++ algoritmos_ordenacao/insertion_sort.hpp
@@ -10,24 +10,6 @@ struct InsertionSortMetrics {
     InsertionSortMetrics() : comparisons(0), swaps(0) {}
 };
 
-void insertionSort(std::vector<int>& arr, InsertionSortMetrics& metrics) {
-    int n = arr.size();
-    for (int i = 1; i < n; ++i) {
-        int key = arr[i];
-        int j = i - 1;
-        while (j >= 0) {
-            metrics.comparisons++;
-            if (arr[j] > key) {
-                arr[j + 1] = arr[j];
-                metrics.swaps++; 
-                j = j - 1;
-            } else {
-                break;
-            }
-        }
-        arr[j + 1] = key;
-    }
-}
 
 #endif 
 
